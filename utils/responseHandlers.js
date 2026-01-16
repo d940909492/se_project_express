278e0d8a8dda28dd333cc06e1327ce4aaba766fd
d940909492 <dong5455993@gmail.com>
2026-01-15 23:30:39 -0500

    Create responseHandlers.js

---
+++ utils/responseHandlers.js
@@ -0,0 +1,31 @@
+function sendJson(res, statusCode, payload) {
+  res.status(statusCode).json(payload);
+}
+
+function sendSuccess(res, data, statusCode = 200) {
+  sendJson(res, statusCode, {
+    success: true,
+    data,
+  });
+}
+
+function sendCreated(res, data, message = "resource created") {
+  sendJson(res, 201, {
+    success: true,
+    message,
+    data,
+  });
+}
+
+function sendError(res, message, statusCode = 500) {
+  sendJson(res, statusCode, {
+    success: false,
+    error: message,
+  });
+}
+
+module.exports = {
+  sendSuccess,
+  sendCreated,
+  sendError,
+};
